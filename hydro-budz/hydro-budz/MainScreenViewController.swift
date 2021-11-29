66b108f014bae01a9533caeb045d6583cabd06c6
Marcos Juarez <marcosjuarez@utexas.edu>
2021-11-28 20:18:18 -0600

    Added a Main Screen and a water daily water amount screen

---
+++ hydro-budz/hydro-budz/MainScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainScreenViewController.swift
+//  Hydro-Budz
+//
+//  Created by Amanda Juarez on 11/28/21.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    @IBOutlet weak var UpdateWaterButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
