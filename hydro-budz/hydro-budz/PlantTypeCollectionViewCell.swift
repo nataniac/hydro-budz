eefe957f23c32295c32e64e244ffc51ea2ab5497
labibachow <78191068+labibachow@users.noreply.github.com>
2021-11-30 18:09:19 -0600

    Add files via upload

---
+++ hydro-budz/hydro-budz/PlantTypeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PlantTypeCollectionViewCell.swift
+//  Profile
+//
+//  Created by Labiba Chowdhury on 11/29/21.
+//
+
+import UIKit
+
+class PlantTypeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var plantImageView: UIImageView!
+    
+//    func setup(with plant: Plant) {
+//        plantImageView.image = plant.image
+    
+}
+
